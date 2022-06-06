8ccf83d2752d27be6c8107c30871a2c4b92f50aa
Ifechi <iamifechi@gmail.com>
2022-06-07 00:41:55 +0100

    fixing deploy erroes

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, /*useRef,*/ useEffect } from 'react';
 import {useNavigate } from "react-router-dom";
 import styled from 'styled-components';
 import {Section, Form, Header} from './Signin';
